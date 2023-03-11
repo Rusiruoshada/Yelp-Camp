5135e9103f35faa75592f915fc3267b85ea1302d
roms <rusirusamaraweera749@gmail.com>
2023-03-11 09:21:17 +0530

    add features edit and update data in DB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const { url } = require("inspector");
 const Campground = require("./models/campground");
+const methodOverride = require('method-override');
 const URL =
   "mongodb+srv://sample1:sample123@cluster0.5er8j14.mongodb.net/?retryWrites=true&w=majority";
 
@@ -23,22 +24,80 @@ const app = express();
 mongoose.set("strictQuery", true);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get('/campgrounds', async(req,res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req,res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
 })
 
-app.get('/campgrounds/:id' , async(req,res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show',{campground})
+app.put('/campgrounds/:id' , async(req,res) => {
+  const {id} = req.params;
+  const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
+  res.redirect(`/campgrounds/${campground._id}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
-
